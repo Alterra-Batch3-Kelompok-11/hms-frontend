b682100120ff3d14ebb5446c4413f804bced08d7
Affan Haidar <affanhaidar232@gmail.com>
2022-12-07 18:30:11 +0700

    Add landing page on routes

--- src/routes/RoutesPage.jsx
+++ src/routes/RoutesPage.jsx
@@ -6,22 +6,26 @@ import DoctorRegistration from '../pages/DoctorRegistration';
 import PatientRegistration from '../pages/PatientRegistration';
 import Login from '../pages/login/Login';
 import Doctors from '../components/Doctor/Doctors';
-import Patient from '../components/ListPasien/Patients';
-import ManageDoctor from '../components/Doctor/ManageDoctor';
+import Patient from '../pages/Patient';
+import LandingPage from '../pages/LandingPage';
+import ManageDoctor from '../pages/Patient';
+import AboutUs from '../pages/AboutUs';
 
 function RoutesPage() {
   return (
     <>
-      {/* <Login /> */}
+      {/* <Login />  */}
+      <LandingPage />
+      {/* <AboutUs /> */}
       <Row>
         <Sidebar />
         <Col xs={10} md={10}>
-            {/* <Dashboard /> */}
-            {/* <DoctorRegistration /> */}
-            {/* <Patient /> */}
-            {/* <PatientRegistration /> */}
-            <ManageDoctor />
-            {/* <Doctors /> */}
+            <Dashboard /> 
+            {/* <DoctorRegistration />  */}
+            <Patient /> 
+            {/* <PatientRegistration /> 
+            <ManageDoctor /> 
+            <Doctors /> */}
         </Col>
       </Row>
     </>
